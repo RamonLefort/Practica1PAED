6d33ce28d50651be343a7268f41dba6d991e107b
NestorFlores <nestor.flores@students.salle.url.edu>
2024-11-13 09:58:28 +0100

    Extraer los datos del archivo y calculador los pesos de cada archivo.

--- weightCalculation.py
+++ weightCalculation.py
@@ -2,12 +2,17 @@ import math
 from datetime import datetime
 
 def calculate_time_weight(limit_year, limit_month, limit_day):
+    # Hacemos un try para el caso que te introduzca una fecha anterior a la actual.
     actual_date = datetime.now().date()  # Fecha actual sin hora
     limit_date = datetime(2000 + limit_year, limit_month, limit_day).date()  # Fecha límite sin hora
 
-    delta_days = max((limit_date - actual_date).days, 1)  # Evitamos división por cero
+    print((limit_date - actual_date).days, max((limit_date - actual_date).days, 1))
 
-    return min(int(1000 / (delta_days ** 0.5)), 500)  # Equivalente a 1000 / (delta_days)^(1/2)
+    delta_days = max((limit_date - actual_date).days, 1)
+
+    return min(int(1000 / (delta_days ** 0.5)), 500)
+
+
 
 def calculate_red_weight_by_hexa(color):
     color = color.lstrip('#')
@@ -18,10 +23,12 @@ def calculate_red_weight_by_hexa(color):
 
 
 def calculate_weight_of_task(limit_year, limit_month, limit_day, color, progress):
-    time_weight = calculate_time_weight(limit_year, limit_month, limit_day)
+    time_weight = calculate_time_weight(int(limit_year), int(limit_month), int(limit_day))
     red_weight = calculate_red_weight_by_hexa(color)
 
-    return time_weight + red_weight + progress
+    return int(time_weight) + int(red_weight) + int(progress)
 
 
 # Ejemplo de uso
+
+
